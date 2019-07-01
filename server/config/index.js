87eccde772207a57f69fe399fdf366fc510277df
Guillem Pascual <guillempascualaventin@gmail.com>
2019-07-01 11:04:34 +0200

    Protect MONGO login info

--- server/config/index.js
+++ server/config/index.js
@@ -8,7 +8,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://dbUser:p@55w0rd@cluster0-n1y1j.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.URLDB = urlDB
